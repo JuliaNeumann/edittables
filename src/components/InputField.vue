0900471e42832420534647884f352dc2d489750d
JuliaNeumann <jn.julianeumann@gmail.com>
2018-01-09 18:25:51 +0100

    add toggling between input and text in table cell

---
+++ src/components/InputField.vue
@@ -0,0 +1,27 @@
+<template>
+    <input @blur="saveNewValue" v-model="inputContent" v-if="type === 'text'" type="text" />
+</template>
+
+<script>
+  export default {
+    name: 'InputCell',
+    props: [
+      'value',
+      'type'
+    ],
+    data: function () {
+      return {
+        inputContent: ''
+      }
+    },
+    methods: {
+      saveNewValue: function () {
+        this.$emit('input', this.inputContent)
+        this.$emit('toggleEditMode')
+      }
+    },
+    created: function () {
+      this.inputContent = this.value
+    }
+  }
+</script>
--- src/components/TableCell.vue
+++ src/components/TableCell.vue
@@ -1,14 +1,31 @@
 <template>
     <td>
-        {{ cell.content }}
+        <span @click="toggleEdit" v-if="!editMode">{{ content }}</span>
+        <InputField @toggleEditMode="toggleEdit" v-if="editMode" v-model="content" :type="cell.type"/>
     </td>
 </template>
 
 <script>
+  import InputField from './InputField'
+
   export default {
     name: 'TableCell',
+    components: {
+      InputField
+    },
     props: [
       'cell'
-    ]
+    ],
+    data: function () {
+      return {
+        editMode: false,
+        content: this.cell.content
+      }
+    },
+    methods: {
+      toggleEdit: function () {
+        this.editMode = !this.editMode
+      }
+    }
   }
 </script>
