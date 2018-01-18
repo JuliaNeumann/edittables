efee8f88a1ed5e708e459bf998d47fc01d8f56c7
JuliaNeumann <jn.julianeumann@gmail.com>
2018-01-18 21:34:04 +0100

    add click outside for closing datepicker

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,10 +1,11 @@
 <template>
     <input @blur="saveNewValue" @keydown.enter="saveNewValue" ref="inputField" v-model="inputContent" v-if="type === 'text'" type="text" />
-    <datepicker v-else-if="type === 'date'" language="de" :inline="true" @selected="saveNewValue"></datepicker>
+    <datepicker v-click-outside="saveNewValue" v-else-if="type === 'date'" language="de" :inline="true" @selected="saveNewValue"></datepicker>
 </template>
 
 <script>
   import Datepicker from 'vuejs-datepicker'
+  import ClickOutside from 'vue-click-outside'
 
   export default {
     name: 'InputCell',
@@ -25,6 +26,7 @@
         if (date && (typeof date.toLocaleDateString === 'function')) {
           this.inputContent = date.toLocaleDateString()
         }
+        this.inputContent = this.inputContent || this.value
         this.$emit('input', this.inputContent)
         this.$emit('stopEditMode')
       }
@@ -34,6 +36,9 @@
         this.inputContent = this.value
         this.$nextTick(() => this.$refs.inputField.focus())
       }
+    },
+    directives: {
+      ClickOutside
     }
   }
 </script>
